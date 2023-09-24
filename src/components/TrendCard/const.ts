4697207c3aeebd2e120494c130b9c9770370d845
corners2wall <akosto4kin@gmail.com>
2023-09-24 20:03:01 +0300

    fix: project build

--- src/components/TrendCard/const.ts
+++ src/components/TrendCard/const.ts
@@ -3,7 +3,10 @@ import Pencil from '../../assets/icons/Pencil.svg?react'
 import Edit from '../../assets/icons/Edit.svg?react'
 import TextBullet from '../../assets/icons/TextBullet.svg?react'
 
-export const MediaTypeMap: Record<MediaType, React.FC> = {
+export const MediaTypeMap: Record<
+  MediaType,
+  React.FC<React.ComponentProps<'svg'> & { title?: string; className?: string }>
+> = {
   ARTICLE: TextBullet,
   BLOG: Pencil,
   PRODUCT: Edit
